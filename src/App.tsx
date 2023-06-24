e7ce5d7f0afff1f0b6221fbb16aff4f3588e21ae
devlopersabbir <devlopersabbir@gmail.com>
2023-06-24 13:24:33 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Container, Heading } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Container maxW="400px" w="400px" p={4} mx="auto">
+      <Heading>Hello world</Heading>
+    </Container>
+  );
+};
+
+export default App;

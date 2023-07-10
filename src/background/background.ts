53e43b44477d1c094c08fbcc8f0f354e69897353
devlopersabbir <devlopersabbir@gmail.com>
2023-07-11 01:11:19 +0600

    updated

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,15 +1,19 @@
-// import { getWebsiteListFromStorage } from "../utils/service";
+import { getWebsiteListFromStorage } from "../utils/service";
 
-chrome.runtime.onMessage.addListener((message, sender: any, sendResponse) => {
-  if (message?.action === "domainName") {
-    if (sender) {
-      const domainNameWithWww =
-        sender && sender?.tab.url ? new URL(sender.tab.url).hostname : "";
-      const domainName = domainNameWithWww.replace(/^www\./i, "").trim();
-      console.log(domainName);
-      sendResponse({ domainName });
-    }
-  }
-  return true;
+// chrome.runtime.onMessage.addListener((message, sender: any, sendResponse) => {
+//   if (message?.action === "domainName") {
+//     if (sender) {
+//       const domainNameWithWww =
+//         sender && sender?.tab.url ? new URL(sender.tab.url).hostname : "";
+//       const domainName = domainNameWithWww.replace(/^www\./i, "").trim();
+//       console.log(domainName);
+//       sendResponse({ domainName });
+//     }
+//   }
+//   return true;
+// });
+
+chrome.webNavigation.onBeforeNavigate.addListener(async (details) => {
+  const websiteList: [] = (await getWebsiteListFromStorage()) as any;
+  // make condition and
 });
-

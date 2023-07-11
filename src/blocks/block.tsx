33c88a3bbe698e92726f2e0ad898a21c853ac603
devlopersabbir <devlopersabbir@gmail.com>
2023-07-11 04:42:50 -0700

    last update

--- src/blocks/block.tsx
+++ src/blocks/block.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./BlockApp.tsx";
+import App from "./BlockApp";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById("blocked") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <Toaster position="top-center" />
